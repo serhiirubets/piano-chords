adaa86aabaca004832a7109f88df513db22f5787
serhiirubets <serhii.rubets@gmail.com>
2021-11-09 09:45:02 +0200

    Add loop play

--- src/components/editor/skeleton-entities/skeleton-wrapper.tsx
+++ src/components/editor/skeleton-entities/skeleton-wrapper.tsx
@@ -11,105 +11,117 @@ import {deepCopy} from "../../../utils/js-utils";
 import {getQuadratNodeDimension} from "../../../utils/rendering-utils";
 
 export interface BlockSchemeSkeletonWrapperProps {
-    index: number;
-    id: string,
-    sheetName: string,
-    sortableListeners?: any;
-    sortableAttributes?: any;
+  index: number;
+  id: string,
+  sheetName: string,
+  sortableListeners?: any;
+  sortableAttributes?: any;
 }
 
 let intervalId;
 
 export const SkeletonWrapper = ({
-                                    index,
-                                    id,
-                                    sortableListeners,
-                                    sortableAttributes,
-                                    sheetName
-                                }: BlockSchemeSkeletonWrapperProps) => {
-    const [isHovered, setIsHovered] = useState<boolean>(false);
-    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  index,
+  id,
+  sortableListeners,
+  sortableAttributes,
+  sheetName
+}: BlockSchemeSkeletonWrapperProps) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [isLoopPlaying, setIsLoopPlaying] = useState<boolean>(false);
 
-    const {settings} = useContext(SettingsContext);
-    const {bars, updateBars} = useContext(BarContext);
+  const {settings} = useContext(SettingsContext);
+  const {bars, updateBars} = useContext(BarContext);
 
+  const handleMouseEnter = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setIsHovered(true)
+  }
 
-    const handleMouseEnter = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        setIsHovered(true)
+  const handleMouseLeave = () => {
+    if (!isLoopPlaying) {
+      setIsHovered(false)
     }
+  }
 
-    const handleMouseLeave = () => {
-        if (!isPlaying) {
-          setIsHovered(false)
-        }
-    }
+  const handleClearButtonClick = () => {
+    const editedBars = [...bars];
+    editedBars.splice(index, 1);
+    updateBars(editedBars);
+  }
 
-    const handleClearButtonClick = () => {
-        const editedBars = [...bars];
-        editedBars.splice(index, 1);
-        updateBars(editedBars);
-    }
+  const handleCopyButtonClick = () => {
+    const barCopy = deepCopy(bars[index]);
+    barCopy.id = uuid();
+    const editedBars = [...bars];
+    editedBars.push(barCopy)
+    updateBars(editedBars);
+  }
 
-    const handleCopyButtonClick = () => {
-        const barCopy = deepCopy(bars[index]);
-        barCopy.id = uuid();
-        const editedBars = [...bars];
-        editedBars.push(barCopy)
-        updateBars(editedBars);
-    }
+  const handlePlayButtonClick = (playFunction) => {
+    playNotes(getNotesToPlay([{
+      data: bars[index],
+      relativePosition: 0
+    }]), playFunction, settings.playbackTempo, settings.alterGainForFeather, settings.barSize)
+  }
 
-    const handlePlayButtonClick = (playFunction) => {
-      setIsPlaying(true);
-      const notes = getNotesToPlay([{
-        data: bars[index],
-        relativePosition: 0
-      }]);
+  const handleLoopClick = (playFunction) => {
+    setIsLoopPlaying(true);
+    const notes = getNotesToPlay([{
+      data: bars[index],
+      relativePosition: 0
+    }]);
 
-      playNotes(notes, playFunction, settings.playbackTempo, settings.alterGainForFeather, settings.barSize);
+    playNotes(notes, playFunction, settings.playbackTempo, settings.alterGainForFeather, settings.barSize);
 
-      intervalId = setInterval(() => {
-        playNotes(notes, playFunction, settings.playbackTempo, settings.alterGainForFeather, settings.barSize)
-      }, notes.length * 1000 * settings.playbackTempo + 1000);
+    intervalId = setInterval(() => {
+      playNotes(notes, playFunction, settings.playbackTempo, settings.alterGainForFeather, settings.barSize)
+    }, notes.length * 1000 * settings.playbackTempo + 1000);
+  }
 
-    }
+  const onStopLoop = () => {
+    clearInterval(intervalId);
+    setIsLoopPlaying(false);
+  }
 
-    return (
-        <div style={{
-            marginTop: "20px",
-            marginLeft: "10px",
-            marginRight: "10px",
-            justifyContent: "center",
-            flexDirection: "column",
-            display: "flex",
-            maxWidth: getQuadratNodeDimension(settings.isMasteringMode).quadratWidth * settings.barSize + 40
-        }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+  return (
+    <div style={{
+      marginTop: "20px",
+      marginLeft: "10px",
+      marginRight: "10px",
+      justifyContent: "center",
+      flexDirection: "column",
+      display: "flex",
+      maxWidth: getQuadratNodeDimension(settings.isMasteringMode).quadratWidth * settings.barSize + 40
+    }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
 
-            <div style={{display: "flex", justifyContent: "flex-end", flexDirection: "row", width: "100%"}}>
-                {isHovered ? <SoundfontProvider
-                        instrumentName={DEFAULT_INSTRUMENT}
-                        audioContext={audioContext}
-                        hostname={soundfontHostname}
-                        render={({playNote, stopNote, stopAllNotes}) => (
-                            <div style={{display: "flex", flexDirection: "row", width: "100%"}}>
-                                <SkeletonWrapperControls onStartPlaying={() => handlePlayButtonClick(playNote)}
-                                                         onStopPlaying={() => {
-                                                             setIsPlaying(false);
-                                                             clearInterval(intervalId);
-                                                             stopNote();
-                                                             stopAllNotes();
-                                                         }}
-                                                         onCopy={handleCopyButtonClick}
-                                                         onClear={handleClearButtonClick}
-                                                         isDisplayed={true}
-                                                         id={id}
-                                                         sortableListeners={sortableListeners}
-                                                         sortableAttributes={sortableAttributes}
-                                />
-                            </div>
-                        )}/>
-                    : (<div style={{height: 44, width: '100%'}}></div>)}
-            </div>
-            <Skeleton skeletonIndex={index} sheetName={sheetName}></Skeleton>
-        </div>)
+      <div style={{display: "flex", justifyContent: "flex-end", flexDirection: "row", width: "100%"}}>
+        {isHovered ? <SoundfontProvider
+            instrumentName={DEFAULT_INSTRUMENT}
+            audioContext={audioContext}
+            hostname={soundfontHostname}
+            render={({playNote, stopNote, stopAllNotes}) => (
+              <div style={{display: "flex", flexDirection: "row", width: "100%"}}>
+                <SkeletonWrapperControls
+                  onStartPlaying={() => handlePlayButtonClick(playNote)}
+                  onLoop={() => handleLoopClick(playNote)}
+                  onStopLoop={onStopLoop}
+                  onStopPlaying={() => {
+                    stopNote();
+                    stopAllNotes();
+                  }}
+                  onCopy={handleCopyButtonClick}
+                  onClear={handleClearButtonClick}
+                  isDisplayed={true}
+                  id={id}
+                  isLoopPlaying={isLoopPlaying}
+                  sortableListeners={sortableListeners}
+                  sortableAttributes={sortableAttributes}
+                />
+              </div>
+            )}/>
+          : (<div style={{height: 44, width: '100%'}}></div>)}
+      </div>
+      <Skeleton skeletonIndex={index} sheetName={sheetName}></Skeleton>
+    </div>)
 }
 

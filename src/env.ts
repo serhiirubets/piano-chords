60609c0e21726a4f8384e07a49756b25e31beb73
serhiirubets <sergey.rubets@wnwd.com>
2022-03-15 09:55:37 +0100

    Test

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,2 @@
 export const REACT_APP_GOOGLE_DRIVE_API_KEY= (process.env.REACT_APP_GOOGLE_DRIVE_API_KEY as string);
 export const REACT_APP_GOOGLE_DRIVE_CLIENT_ID= (process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID as string);
-git
